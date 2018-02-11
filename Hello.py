c2a2080ce57cb00c0f0ef6f2b054e02531b23f11
Pengyi Mu <mupengyi@foxmail.com>
2018-02-11 13:00:05 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,5 +1,10 @@
+import re
+
 string = 'h' + 'e' + 'l' + 'l' + 'o' + ',' + 'w' + 'o' + 'r' + 'l' + 'd'
 print(string)
 
 string = 'h232e3dsl423lfs2oeqw,234wffwo432r432l43fd555'[::4]
+print(string)
+
+string = re.sub(r'^(helo,wrd)',r'',string)
 print(string)
