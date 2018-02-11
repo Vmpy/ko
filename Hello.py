445565ca799e50993e57f9b5ce4a069c681effc0
Pengyi Mu <mupengyi@foxmail.com>
2018-02-11 13:01:01 +0800

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -6,5 +6,6 @@ print(string)
 string = 'h232e3dsl423lfs2oeqw,234wffwo432r432l43fd555'[::4]
 print(string)
 
+string = 'h232e3dsl423lfs2oeqw,234wffwo432r432l43fd555'
 string = re.sub(r'^(helo,wrd)',r'',string)
 print(string)
